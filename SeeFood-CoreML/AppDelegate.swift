2d3c94f0701091f9c6f0231dc1a43238abe26fd2
zloysergunya <zloysergunya@mail.ru>
2021-10-31 22:05:23 +0700

    Update for SFU lab

--- SeeFood-CoreML/AppDelegate.swift
+++ SeeFood-CoreML/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  SeeFood-CoreML
-//
-//  Created by Angela Yu on 27/06/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
